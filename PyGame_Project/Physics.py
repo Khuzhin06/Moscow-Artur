0b89b999c5ae53ea1c23f795b7aa2efa2381424f
Artur <khuzhin06@mail.ru>
2022-12-25 21:49:43 +0300

    protip v1

--- PyGame_Project/Physics.py
+++ PyGame_Project/Physics.py
@@ -3,9 +3,10 @@ from random import choice
 
 FPS = 50  # количество кадров в секунду
 SIZE = WIDTH, HEIGHT = 800, 400
+FONT_NAME = pygame.font.match_font('arial')
 
 
-class Board:
+class Physic:
     # создание поля
     def __init__(self, width):
         self.width = width
@@ -17,6 +18,7 @@ class Board:
         self.top = 10
         self.cell_size = 30
         self.live = 3
+        self.point = 0
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
@@ -24,7 +26,7 @@ class Board:
         self.top = top
         self.cell_size = cell_size
 
-    def render(self,  screen):
+    def render(self, screen):
         end_x = self.width * self.cell_size + self.left
         for x in range(self.left, end_x, self.cell_size):
             col = (x - self.left) // self.cell_size
@@ -109,27 +111,37 @@ class Board:
 
     def turn_left(self):
         choi = choice(['a', 'w', 'd', 's'])
+        if self.board[0] in ('d', 'w', 's', 'a'):
+            self.live -= 1
         self.board.pop(0)
         self.board.append(choi)
 
     def click(self, a):
-        print(a)
         if a == self.board[0]:
             self.board[0] = 'Yes'
-        elif self.board[0] != 0 and self.board[0] != 'Yes':
+            self.point += 10
+        elif self.board[0] != 0 and self.board[0] != 'Yes' and self.board[0] != 'No':
             self.board[0] = 'No'
+            self.live -= 1
 
 
-def main():
-    pygame.display.set_caption('Клетчатое поле начало')
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(FONT_NAME, size)
+    text_surface = font.render(text, True, 'WHITE')
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
+
+def physic():
+    pygame.display.set_caption('химичка')
     screen = pygame.display.set_mode(SIZE)
 
     clock = pygame.time.Clock()
     running = True
-    turn = 0  # Нужно для подсчета скорости движения стрелок
 
     # поле 5 на 7
-    board = Board(10)
+    board = Physic(10)
     board.set_view(0, 320, 80)
     TURN_BOARD_EVENT = pygame.USEREVENT + 1
     TURN_BOARD_TIMER = 1000
@@ -142,8 +154,8 @@ def main():
             if event.type == TURN_BOARD_EVENT:
                 board.turn_left()
                 TURN_BOARD_TIMER -= 30
-                if TURN_BOARD_TIMER <= 200:
-                    TURN_BOARD_TIMER = 200
+                if TURN_BOARD_TIMER <= 500:
+                    TURN_BOARD_TIMER = 500
                 pygame.time.set_timer(TURN_BOARD_EVENT, TURN_BOARD_TIMER)
 
         keys = pygame.key.get_pressed()
@@ -156,7 +168,11 @@ def main():
         elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
             board.click('s')
 
+        if not board.live:
+            running = False
         screen.fill('black')
+        # screen.blit(background, background_rect)
+        draw_text(screen, str(board.point), 18, WIDTH / 2, 10)
         board.render(screen)
         pygame.display.flip()
         clock.tick(FPS)
@@ -164,6 +180,5 @@ def main():
 
 if __name__ == '__main__':
     pygame.init()
-    main()
+    physic()
     pygame.quit()
-
